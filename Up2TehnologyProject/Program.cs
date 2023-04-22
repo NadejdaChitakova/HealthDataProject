7d5d9b04bd68e29c3324028c79ad23792f4d476f
NadezhdaChitakova <Nadejda.Chitakova@sebrands.com>
2023-04-22 21:17:51 +0300

    Introduce swagger

--- Up2TehnologyProject/Program.cs
+++ Up2TehnologyProject/Program.cs
@@ -1,18 +1,24 @@
+using Up2TehnologyProject.IRepository;
+using Up2TehnologyProject.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-
+builder.Services.AddScoped<IHealthRepository, HealthRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+builder.Services.AddHttpClient();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
